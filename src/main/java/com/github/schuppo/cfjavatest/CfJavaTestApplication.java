64d7396d83224967ff4a828a5533d74ac8f0fcef
Oliver Schupp <oliver.schupp@idealo.de>
2019-02-23 21:09:29 +0100

    adds sb

---
+++ src/main/java/com/github/schuppo/cfjavatest/CfJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.schuppo.cfjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfJavaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/schuppo/cfjavatest/CfJavaTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.schuppo.cfjavatest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CfJavaTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
